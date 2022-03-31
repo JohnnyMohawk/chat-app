cc8ad5673b3688bd294f4fa84e059022a1914bc2
JohnnyMohawk <johnnelsonalden@gmail.com>
2022-03-30 22:18:18 -0500

    set up sockets

--- server/src/app.ts
+++ server/src/app.ts
@@ -7,6 +7,8 @@ import config from 'config';
 import logger from './utils/logger';
 import { version } from '../package.json';
 
+import socket from './socket';
+
 const port = config.get<number>("port");
 const host = config.get<string>("host");
 const corsOrigin = config.get<string>("corsOrigin");
@@ -28,4 +30,6 @@ app.get('/', (_, res) => res.send(`Server is up version ${version}`))
 httpServer.listen(port, host, () => {
     logger.info(`🚀 Server version ${version} is listening 🚀`);
     logger.info(`http://${host}:${port}`);
+
+    socket({ io });
 });
